890a3b7a1218a9e3d0c97d21aa9e13dbc4ac6145
Stanislas Azanmassou <stanleylab97@github.com>
2020-05-09 19:00:36 +0100

    Initialisation du  projet

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('pages/index.html.twig', [
+            'controller_name' => 'PagesController',
+        ]);
+    }
+
+      /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('pages/contact.html.twig', [
+            'controller_name' => 'PagesController',
+        ]);
+    }
+
+
+      /**
+     * @Route("/services", name="services")
+     */
+    public function services()
+    {
+        return $this->render('pages/services.html.twig', [
+            'controller_name' => 'PagesController',
+        ]);
+    }
+}
